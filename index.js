28f9bff87ef7383b0db3142c413757f061dacfad
Sylvain Gandon <krack_6@hotmail.com>
2018-08-25 00:02:42 +0200

    use https api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
- /* jshint -W082:true */
+/* jshint -W082:true */
 /*
 Setup:
   npm install ws
@@ -11,7 +11,7 @@ Usage:
 */
 var WebSocket = require('ws');
 var http = require('http');
-var proxy = require('redbird')({port: 80});
+var proxy = require('redbird')({ port: 80 });
 
 var host = process.env.RANCHER_HOST;
 var accessKey = process.env.RANCHER_ACCESS_KEY;
@@ -19,88 +19,87 @@ var secretKey = process.env.RANCHER_SECRET_KEY;
 
 var URL_LABEL = "proxy_url";
 
-var urlEvent = 'ws://'+accessKey+':'+secretKey+'@'+host+'/v1/subscribe?eventNames=resource.change';
+var urlEvent = 'ws://' + accessKey + ':' + secretKey + '@' + host + '/v1/subscribe?eventNames=resource.change';
 
 
-function callApi(path, callback){
+function callApi(path, callback) {
 	var apiHost = "localhost";
 	var portHost = 80;
 
 
 	var hostPart = host.split(':');
-	if(hostPart.length ==2){
-		apiHost = hostPart[0];	
-		portHost = hostPart[1];	
-	}else{
+	if (hostPart.length == 2) {
+		apiHost = hostPart[0];
+		portHost = hostPart[1];
+	} else {
 		apiHost = host;
 	}
 	var options = {
 		host: apiHost,
-		port : portHost,
+		port: portHost,
 		path: '/v1/containers',
-		auth: accessKey+':'+secretKey
+		auth: accessKey + ':' + secretKey
 	};
-	
-	http.get(options, function(res) {
-		  // Continuously update stream with data
-        var body = '';
-        res.on('data', function(d) {
-            body += d;
-        });
-        res.on('end', function() {
 
-            // Data reception is done, do whatever with it!
-            var parsed = JSON.parse(body);
-            callback(parsed);
-        });
-	}).on('error', function(e) {
+	http.get(options, function (res) {
+		// Continuously update stream with data
+		var body = '';
+		res.on('data', function (d) {
+			body += d;
+		});
+		res.on('end', function () {
+
+			// Data reception is done, do whatever with it!
+			var parsed = JSON.parse(body);
+			callback(parsed);
+		});
+	}).on('error', function (e) {
 		console.log(e);
 	});
 }
 
-function manageContainer(container, ignoreStoppped){
-	if(container.labels[URL_LABEL]){
-		try{
-	    	var configurations = extractConfig(container);
+function manageContainer(container, ignoreStoppped) {
+	if (container.labels[URL_LABEL]) {
+		try {
+			var configurations = extractConfig(container);
 
-	    	for(var i = 0; i < configurations.length; i++){
-	    		var config = configurations[i];
-		    	//remove configuration where stopped
-		    	if(!ignoreStoppped && container.state === 'stopping')
-		    	{
-		    		console.log("remove config for "+config.serverName+"/ : "+config.serverRedirect+":"+config.serverRedirectPort);
-		    		 proxy.unregister(config.serverName+"/", config.serverRedirect+":"+config.serverRedirectPort);
-		    	}
+			for (var i = 0; i < configurations.length; i++) {
+				var config = configurations[i];
+				//remove configuration where stopped
+				if (!ignoreStoppped && container.state === 'stopping') {
+					console.log("remove config for " + config.serverName + "/ : " + config.serverRedirect + ":" + config.serverRedirectPort);
+					proxy.unregister(config.serverName + "/", config.serverRedirect + ":" + config.serverRedirectPort);
+				}
 				//create configuration where stopper
-		    	else if(container.state === 'running'){
-		    		console.log("add config for "+config.serverName+" : "+config.serverRedirect+":"+config.serverRedirectPort);
-					proxy.register(config.serverName, "http://"+config.serverRedirect+":"+config.serverRedirectPort);
-		    	}
-		    }
-	    }catch(e){
-	    	console.log(e);
-	    }
-    }
+				else if (container.state === 'running') {
+					console.log("add config for " + config.serverName + " : " + config.serverRedirect + ":" + config.serverRedirectPort);
+					proxy.register(config.serverName, "https://" + config.serverRedirect + ":" + config.serverRedirectPort);
+				}
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	}
 }
-function extractConfig(resource){
+function extractConfig(resource) {
 	var configurations = [];
 
 	var config = {};
-	config.serverName = resource.labels[URL_LABEL];	
+	config.serverName = resource.labels[URL_LABEL];
 	var serviceName = resource.labels['io.rancher.stack_service.name'];
-	var dns = serviceName.split("/")[1]+"."+serviceName.split("/")[0];
+	var dns = serviceName.split("/")[1] + "." + serviceName.split("/")[0];
 	config.serverRedirect = dns;
 	var allInfo = resource.ports[0];
-	if(allInfo) {
+	if (allInfo) {
 		config.serverRedirectPort = allInfo.split(":")[1].split("/")[0];
 	}
 
 	configurations.push(config);
 
 	//if is production configuration, add url without www.
-	if(config.serverName.indexOf("www.")===0){
+	if (config.serverName.indexOf("www.") === 0) {
 		var prodConfig = {};
-		prodConfig.serverName = config.serverName.replace("www.", ""); 
+		prodConfig.serverName = config.serverName.replace("www.", "");
 		prodConfig.serverRedirect = config.serverRedirect;
 		prodConfig.serverRedirectPort = config.serverRedirectPort;
 
@@ -109,40 +108,40 @@ function extractConfig(resource){
 
 
 	return configurations;
-	
+
 }
-function addRunningServer(){
-	callApi('/v1/containers', function(result){
-		for(var i = 0; i < result.data.length; i++){
+function addRunningServer() {
+	callApi('/v1/containers', function (result) {
+		for (var i = 0; i < result.data.length; i++) {
 			var container = result.data[i];
 			manageContainer(container, true);
 		}
 	});
 }
 
-function runEventListener(){
+function runEventListener() {
 	var socket = new WebSocket(urlEvent);
 
-	socket.on('open', function() {
-	  console.log('Socket opened');
+	socket.on('open', function () {
+		console.log('Socket opened');
 	});
 
-	socket.on('message', function(messageStr) {
-	  var message = JSON.parse(messageStr);
-	  if ( message.name === 'resource.change') {
-	  	if( message.resourceType === 'container'){
-		   	if(message.data){
-			    var resource = message.data.resource;
-			    manageContainer(resource, false);
-			}		
+	socket.on('message', function (messageStr) {
+		var message = JSON.parse(messageStr);
+		if (message.name === 'resource.change') {
+			if (message.resourceType === 'container') {
+				if (message.data) {
+					var resource = message.data.resource;
+					manageContainer(resource, false);
+				}
+			}
 		}
-	  }
 	});
 
-	socket.on('close', function() {
-	  console.log('Socket closed');
-	  //relauch where is close
-	  runEventListener();
+	socket.on('close', function () {
+		console.log('Socket closed');
+		//relauch where is close
+		runEventListener();
 	});
 }
 
